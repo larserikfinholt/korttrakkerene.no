ee47ec56f0cffe1e4f87a7c63b84f084d91c8ff1
Lars Erik Finholt <lars.erik.finholt@bouvet.no>
2021-01-11 20:34:58 +0100

    minor

--- web/gridsome.config.js
+++ web/gridsome.config.js
@@ -13,9 +13,9 @@ const clientConfig = require('./client-config')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
+  siteName: 'Korttråkkerne',
   siteDescription:
-    'A simple, hackable & minimalistic starter for Gridsome that uses structured content from Sanity.io.',
+    'Korttråkkerne sykler på sti i Gulsetmarka.',
 
   templates: {
     SanityPost: '/:slug__current'
